8979e62123deba43d01e86ff803c77bb7bd5b929
FaisalAli <faisalali1901@gmail.com>
2022-11-03 20:08:45 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import '../styles/globals.css';
+
+const theme = extendTheme({ colors: {}, fonts: {} });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
