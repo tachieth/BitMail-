d8e71196f52b1fb0bdde11f91ca3f4907609ad32
qimran1234 <qimrankhan94@gmail.com>
2022-11-05 12:13:33 +0530

    new changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import '../styles/globals.css';
+import '@fontsource/roboto';
 
-const theme = extendTheme({ colors: {}, fonts: {} });
+const theme = extendTheme({
+  colors: {
+    bg: '#42367c',
+  },
+  fonts: {
+    heading: "'Roboto', sans-serif",
+    body: "'Roboto', sans-serif",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
